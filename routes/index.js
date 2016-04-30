e58a042194f32ea0f17dfdedd2c4c4194308635e
adekola <adekola.m.adebayo@gmail.com>
2016-05-01 01:41:58 +0200

    a better fix for the webhook ish

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+router.get('/webhook/', function (req, res) {
+  if (req.query['hub.verify_token'] === 'well_you_know_who_it_is') {
+    res.send(req.query['hub.challenge']);
+  }
+  res.send('Error, wrong validation token');
+})
+
 module.exports = router;
